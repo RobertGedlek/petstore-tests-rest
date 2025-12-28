d7893dff51165ee05b7cb0c8270df2b8fd0f5ea4
Robert Gedlek <robertge@backbase.com>
2025-12-28 12:50:21 +0100

    Refactor BaseTest to initialize PetStoreApiClient in a setup method

--- src/test/java/info/gedlek/tests/BaseTest.java
+++ src/test/java/info/gedlek/tests/BaseTest.java
@@ -1,8 +1,15 @@
 package info.gedlek.tests;
 
 import info.gedlek.api.PetStoreApiClient;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class BaseTest {
-    protected final PetStoreApiClient petApiClient = new PetStoreApiClient();
+    protected PetStoreApiClient petApiClient;
 
+    @BeforeAll
+    void setup() {
+        petApiClient = new PetStoreApiClient();
+    }
 }
