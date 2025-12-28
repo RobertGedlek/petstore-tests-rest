6e8e880defd308b0a7169b96a96c813f7eaf715e
Robert Gedlek <robertge@backbase.com>
2025-12-28 12:23:32 +0100

    Add parameterized test for creating pets with different statuses

--- src/test/java/info/gedlek/tests/PetApiTest.java
+++ src/test/java/info/gedlek/tests/PetApiTest.java
@@ -1,10 +1,12 @@
 package info.gedlek.tests;
 
-import info.gedlek.api.PetStoreApiClient;
 import info.gedlek.model.Category;
+import info.gedlek.model.Pet;
 import info.gedlek.utils.TestDataGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static info.gedlek.asserters.PetAsserter.assertThat;
 
@@ -38,5 +40,22 @@ class PetApiTest extends  BaseTest {
                 .toHaveCategory(category)
                 .toHavePhotoUrls(pet.getPhotoUrls())
                 .toHaveStatus(pet.getStatus());
+    }
+
+
+    @ParameterizedTest(name = "Run #{index}: Create pet with status ''{0}''")
+    @EnumSource(Pet.StatusEnum.class)
+    void shouldCreatePetWithDifferentStatuses(Pet.StatusEnum status) {
+        // given
+        var pet = TestDataGenerator.generateDefaultPet();
+        pet.setStatus(status);
+
+        // when
+        var createdPet = petApiClient.createPet(pet);
+
+        // then
+        assertThat(createdPet)
+                .toHaveStatus(status)
+                .toHaveName(pet.getName());
     }
 }
